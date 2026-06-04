6d85f1aa5c21e6277e32113c7a04988fa409b928
James M.C. Haver II <mchaver@gmail.com>
2026-06-04 14:14:32 +0200

    Improve fonts

--- site.hs
+++ site.hs
@@ -138,6 +138,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "fonts/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
